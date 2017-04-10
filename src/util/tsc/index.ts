41b2b4ee26b59be5c83421eb77a7ddf988d42b9f
Lion <maolion.j@gmail.com>
2017-04-11 03:44:29 +0800

    add report diagnostic for custom tsc

--- src/util/tsc/index.ts
+++ src/util/tsc/index.ts
@@ -1,11 +1,15 @@
+import * as Chalk from 'chalk';
 import * as FS from 'fs-extra';
 import * as Path from 'path';
+
 import {
   CompilerHost,
   CompilerOptions,
   Diagnostic,
+  DiagnosticCategory,
   ExitStatus,
   FileWatcher,
+  FormatDiagnosticsHost,
   Node,
   ParsedCommandLine,
   Program,
@@ -13,12 +17,20 @@ import {
   SourceFile as _SourceFile,
   createCompilerHost,
   createProgram,
+  flattenDiagnosticMessageText,
+  formatDiagnostics,
+  getLineAndCharacterOfPosition,
+  getPositionOfLineAndCharacter,
   parseConfigFileTextToJson,
   parseJsonConfigFileContent,
   sys
 } from 'typescript';
 
 import { readFile } from '../fs';
+
+import {
+  getRelativePath
+} from './helper';
 
 import transformPathMapping from './transform-path-mapping';
 
@@ -37,14 +49,32 @@ export interface TSCOptions {
 }
 
 export interface SourceFile extends _SourceFile {
-    fileWatcher?: FileWatcher;
+  fileWatcher?: FileWatcher;
 }
 
 type ErrorHandler = (message: string) => void;
 
+const redForegroundEscapeSequence = '\u001b[91m';
+const yellowForegroundEscapeSequence = '\u001b[93m';
+const blueForegroundEscapeSequence = '\u001b[93m';
+const gutterStyleSequence = '\u001b[100;30m';
+const gutterSeparator = ' ';
+const resetEscapeSequence = '\u001b[0m';
+const ellipsis = '...';
+
+/** 自定义 tsc, 主要用于完成 path mapping 路径映射到实际的关联文件模块需求 */
 export default function tsc(configFileName: string, options: TSCOptions = {}) {
   const watchSet = !!options.watch;
   const cwd = options.cwd || sys.getCurrentDirectory();
+  const defaultFormatDiagnosticsHost: FormatDiagnosticsHost = {
+    getCurrentDirectory: () => sys.getCurrentDirectory(),
+    getNewLine: () => sys.newLine,
+    getCanonicalFileName: (
+      sys.useCaseSensitiveFileNames ?
+        (fileName => fileName) :
+        (fileName => fileName.toLowerCase())
+    )
+  };
 
   let cachedConfigFileText: string | undefined;
   let compilerOptions: CompilerOptions;
@@ -59,7 +89,12 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
 
   let cachedExistingFiles: Map<string, boolean>;
   let hostFileExists: typeof compilerHost.fileExists;
+  let reportDiagnosticWorker = reportDiagnosticSimply;
 
+  configFileName = Path.resolve(cwd, configFileName);
+
+  // 监听 tsconfig.json 和 对应目录ts项目源代码目录的文件变动情况
+  // 监听文件变通主要是触发实时编译的需求
   if (watchSet) {
     if (sys.watchFile) {
       configFileWatcher = sys.watchFile(configFileName, configFileChanged);
@@ -77,27 +112,37 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
   performCompilation();
 
   function parseConfigFile() {
+
     if (!cachedConfigFileText) {
       try {
         cachedConfigFileText = readFile(configFileName);
       } catch (e) {
-        // TODO: report read config file exception
-        console.error('read config file exception');
+        reportWatchDiagnostic({
+          messageText: `Can not read file ${configFileName}: ${e.message}.`
+        } as Diagnostic);
+
         sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
       }
     }
 
     if (!cachedConfigFileText) {
-      console.error('read config file exception');
+      reportWatchDiagnostic({
+        messageText: `File ${configFileName} not found.`
+      } as Diagnostic);
+
       sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
       return;
     }
 
+    // 解析 项目配置文件, 修正编译参数配置，搜索所有编译目标文件
+
     const result = parseConfigFileTextToJson(configFileName, cachedConfigFileText);
     const configObject = result.config;
-
     if (!configObject) {
-      console.error(result.error);
+      if (result.error) {
+        reportDiagnostics([result.error]);
+      }
+
       sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
       return;
     }
@@ -109,7 +154,7 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
     );
 
     if (configParseResult.errors.length > 0) {
-      console.error(configParseResult.errors);
+      reportDiagnostics(configParseResult.errors);
       sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
     }
 
@@ -117,6 +162,7 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
   }
 
   function performCompilation() {
+
     if (!cachedProgram) {
       const configParseResult = (parseConfigFile() as ParsedCommandLine);
       rootFileNames = configParseResult.fileNames;
@@ -130,6 +176,10 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
       compilerHost.fileExists = cachedFileExists;
     }
 
+    if (compilerOptions.pretty) {
+      reportDiagnosticWorker = reportDiagnosticPretty;
+    }
+
     cachedExistingFiles = new Map<string, boolean>();
 
     const compileResult = compile(rootFileNames, compilerOptions, compilerHost);
@@ -140,8 +190,9 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
 
     setCachedProgram(compileResult.program);
 
-    // TODO: report watching file change
-    console.log('watching... file change...');
+    reportWatchDiagnostic({
+      messageText: 'Compilation complete. Watching for file changes.'
+    } as Diagnostic);
   }
 
   function cachedFileExists(fileName: string): boolean {
@@ -157,6 +208,8 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
   function getSourceFile(fileName: string, languageVersion: ScriptTarget, onError?: ErrorHandler): SourceFile {
     if (cachedProgram) {
       const sourceFile = cachedProgram.getSourceFile(fileName) as SourceFile;
+
+      // 使用已被处理过的文件缓存数据
       if (sourceFile && sourceFile.fileWatcher) {
         return sourceFile;
       }
@@ -174,24 +227,25 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
   }
 
   function setCachedProgram(program: Program | undefined) {
-      const newSourceFiles = program ? program.getSourceFiles() : undefined;
+    const newSourceFiles = program ? program.getSourceFiles() : undefined;
 
-      if (cachedProgram && newSourceFiles) {
-        const sourceFiles = cachedProgram.getSourceFiles() as SourceFile[];
+    if (cachedProgram && newSourceFiles) {
+      const sourceFiles = cachedProgram.getSourceFiles() as SourceFile[];
 
-        sourceFiles.forEach(sourceFile => {
-          if (contains(newSourceFiles, sourceFile)) {
-            return;
-          }
+      sourceFiles.forEach(sourceFile => {
+        if (contains(newSourceFiles, sourceFile)) {
+          return;
+        }
 
-          if (sourceFile.fileWatcher) {
-              sourceFile.fileWatcher.close();
-              sourceFile.fileWatcher = undefined;
-          }
-        });
-      }
+        // 不在编译目标文件范围内的老文件缓存数据被清掉
+        if (sourceFile.fileWatcher) {
+          sourceFile.fileWatcher.close();
+          sourceFile.fileWatcher = undefined;
+        }
+      });
+    }
 
-      cachedProgram = program;
+    cachedProgram = program;
   }
 
   function sourceFileChanged(sourceFile: SourceFile, removed?: boolean) {
@@ -208,6 +262,7 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
   }
 
   function configFileChanged() {
+    // 因为项目配置文件的改动，所以 不在使用缓存数据
     setCachedProgram(undefined);
     cachedConfigFileText = undefined;
 
@@ -239,7 +294,7 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
 
   function startTimerForRecompilation() {
     if (timerHandleForRecompilation) {
-        clearTimeout(timerHandleForRecompilation);
+      clearTimeout(timerHandleForRecompilation);
     }
 
     timerHandleForRecompilation = setTimeout(recompile, 250);
@@ -247,71 +302,209 @@ export default function tsc(configFileName: string, options: TSCOptions = {}) {
 
   function recompile() {
     timerHandleForRecompilation = undefined;
-    console.log('recompile');
+
+    reportWatchDiagnostic({
+      messageText: 'File change detected. Starting incremental compilation...'
+    } as Diagnostic);
+
     performCompilation();
+  }
+
+  function compile(fileNames: string[], compilerOptions: CompilerOptions, compilerHost: CompilerHost) {
+    const program = createProgram(fileNames, compilerOptions, compilerHost);
+    const exitStatus = compilerProgram();
+
+    return {
+      program,
+      exitStatus
+    };
+
+    function compilerProgram() {
+      let diagnostics: Diagnostic[];
+      diagnostics = program.getSyntacticDiagnostics();
+
+      if (diagnostics.length === 0) {
+        diagnostics = program.getOptionsDiagnostics().concat(program.getGlobalDiagnostics());
+
+        if (diagnostics.length === 0) {
+          diagnostics = program.getSemanticDiagnostics();
+        }
+      }
+
+      const emitOutput = program.emit(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        {
+          after: [
+            transformPathMapping
+          ]
+        }
+      );
+
+      diagnostics = diagnostics.concat(emitOutput.diagnostics);
+
+      reportDiagnostics(diagnostics);
+
+      if (emitOutput.emitSkipped && diagnostics.length > 0) {
+        return ExitStatus.DiagnosticsPresent_OutputsSkipped;
+      } else if (diagnostics.length > 0) {
+        return ExitStatus.DiagnosticsPresent_OutputsGenerated;
+      }
+
+      return ExitStatus.Success;
+    }
+  }
+
+  function reportDiagnostic(diagnostic: Diagnostic, host?: FormatDiagnosticsHost) {
+    reportDiagnosticWorker(diagnostic, host || defaultFormatDiagnosticsHost);
+  }
+
+  function reportDiagnostics(diagnostics: Diagnostic[], host?: FormatDiagnosticsHost): void {
+    for (const diagnostic of diagnostics) {
+      reportDiagnostic(diagnostic, host);
+    }
   }
 }
 
-function compile(fileNames: string[], compilerOptions: CompilerOptions, compilerHost: CompilerHost) {
-  const program = createProgram(fileNames, compilerOptions, compilerHost);
-  const exitStatus = compilerProgram();
+function reportDiagnosticSimply(diagnostic: Diagnostic, host: FormatDiagnosticsHost) {
+  sys.write(formatDiagnostics([diagnostic], host));
+}
 
-  return {
-    program,
-    exitStatus
-  };
+function formatAndReset(text: string, formatStyle: string) {
+  return formatStyle + text + resetEscapeSequence;
+}
 
-  function compilerProgram() {
-    let diagnostics: Diagnostic[];
-    diagnostics = program.getSyntacticDiagnostics();
+function diagnosticCategoryText(category: DiagnosticCategory): string {
+  const text = DiagnosticCategory[category].toLowerCase();
 
-    if (diagnostics.length === 0) {
-      diagnostics = program.getOptionsDiagnostics().concat(program.getGlobalDiagnostics());
+  let colorWrapper: (text?: string) => string = String;
 
-      if (diagnostics.length === 0) {
-        diagnostics = program.getSemanticDiagnostics();
-      }
-    }
-
-    const emitOutput = program.emit(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      {
-        after: [
-          transformPathMapping
-        ]
-      }
-    );
-
-    diagnostics = diagnostics.concat(emitOutput.diagnostics);
-
-    // TODO: report diagnostics
-    console.log(diagnostics);
-
-    if (emitOutput.emitSkipped && diagnostics.length > 0) {
-      return ExitStatus.DiagnosticsPresent_OutputsSkipped;
-    } else if (diagnostics.length > 0) {
-      return ExitStatus.DiagnosticsPresent_OutputsGenerated;
-    }
-
-    return ExitStatus.Success;
+  switch (category) {
+    case DiagnosticCategory.Warning: colorWrapper = Chalk.yellow; break;
+    case DiagnosticCategory.Error: colorWrapper = Chalk.red; break;
+    case DiagnosticCategory.Message: colorWrapper = Chalk.blue; break;
   }
 
+  return colorWrapper(text);
+}
+
+function reportDiagnosticPretty(diagnostic: Diagnostic, host: FormatDiagnosticsHost) {
+  let output: string = '';
+
+  // 排版 出错位置定位 打印信息
+  if (diagnostic.file) {
+    const { start, length, file } = diagnostic;
+    const { line: firstLine, character: firstLineChar } = getLineAndCharacterOfPosition(file, start);
+    const { line: lastLine, character: lastLineChar } = getLineAndCharacterOfPosition(file, start + length);
+    const lastLineInFile = getLineAndCharacterOfPosition(file, file.text.length).line;
+    const relativeFileName = host ? getRelativePath(host.getCurrentDirectory(), file.fileName) : file.fileName;
+
+    const hasMoreThanFiveLines = (lastLine - firstLine) >= 4;
+    let gutterWidth = (lastLine + 1 + '').length;
+
+    if (hasMoreThanFiveLines) {
+      gutterWidth = Math.max(ellipsis.length, gutterWidth);
+    }
+
+    output += NL;
+    for (let i = firstLine; i <= lastLine; i++) {
+      if (hasMoreThanFiveLines && firstLine + 1 < i && i < lastLine - 1) {
+        output +=
+          (formatAndReset(padLeft(ellipsis, gutterWidth), gutterStyleSequence)
+            + gutterSeparator
+            + NL);
+
+        i = lastLine - 1;
+      }
+
+      const lineStart = getPositionOfLineAndCharacter(file, i, 0);
+      const lineEnd = i < lastLineInFile ? getPositionOfLineAndCharacter(file, i + 1, 0) : file.text.length;
+      let lineContent = file.text.slice(lineStart, lineEnd);
+      lineContent = lineContent.replace(/\s+$/g, '');
+      lineContent = lineContent.replace('\t', ' ');
+
+      output +=
+        (`\u001b[100;30m${padLeft(i + 1 + '', gutterWidth)}\u001b[0m`
+          + gutterSeparator
+          + lineContent
+          + NL
+          // ----
+          + formatAndReset(padLeft('', gutterWidth), gutterStyleSequence)
+          + gutterSeparator);
+
+      output += redForegroundEscapeSequence;
+      if (i === firstLine) {
+        const lastCharForLine = i === lastLine ? lastLineChar : undefined;
+
+        output += lineContent.slice(0, firstLineChar).replace(/\S/g, ' ');
+        output += lineContent.slice(firstLineChar, lastCharForLine).replace(/./g, '~');
+      } else if (i === lastLine) {
+        output += lineContent.slice(0, lastLineChar).replace(/./g, '~');
+      } else {
+        output += lineContent.replace(/./g, '~');
+      }
+      output += resetEscapeSequence;
+
+      output += sys.newLine;
+    }
+
+    output += sys.newLine;
+    output += `${relativeFileName}(${firstLine + 1},${firstLineChar + 1}): `;
+  }
+
+  output +=
+    (`${diagnosticCategoryText(diagnostic.category)} `
+      + `TS${diagnostic.code} ${flattenDiagnosticMessageText(diagnostic.messageText, NL)}`
+      + NL
+      + NL);
+
+  sys.write(output);
+}
+
+function reportEmittedFiles(files: string[]): void {
+  if (!files || files.length === 0) {
+    return;
+  }
+
+  const currentDir = sys.getCurrentDirectory();
+
+  for (const file of files) {
+    const filepath = Path.resolve(currentDir, file);
+
+    sys.write(`TSFILE: ${filepath}${NL}`);
+  }
+}
+
+function reportWatchDiagnostic(diagnostic: Diagnostic) {
+  let output = new Date().toLocaleTimeString() + ' - ';
+
+  if (diagnostic.file) {
+    const loc = getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
+    output += `${diagnostic.file.fileName}(${loc.line + 1},${loc.character + 1}): `;
+  }
+
+  output +=
+    (`${flattenDiagnosticMessageText(diagnostic.messageText, NL)}`
+      + NL
+      + NL
+      + NL);
+
+  sys.write(output);
 }
 
 // Helpers
 
 function contains<T>(array: T[], value: T): boolean {
-    if (array) {
-        for (const v of array) {
-            if (v === value) {
-                return true;
-            }
-        }
+  if (array) {
+    for (const v of array) {
+      if (v === value) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function arrayIsEqualTo<T>(array1: T[], array2: T[], equaler?: (a: T, b: T) => boolean): boolean {
@@ -349,4 +542,19 @@ function isSupportedSourceFileName(fileName: string) {
   }
 
   return false;
+}
+
+function padLeft(s: string, length: number) {
+  while (s.length < length) {
+    s = ' ' + s;
+  }
+  return s;
+}
+
+function padRight(s: string, length: number) {
+  while (s.length < length) {
+    s = s + ' ';
+  }
+
+  return s;
 }
--- src/util/tsc/transform-path-mapping.ts
+++ src/util/tsc/transform-path-mapping.ts
@@ -34,11 +34,14 @@ export default function transformPathMapping(context: TransformationContext) {
   let needSubstitutionModulePath = false;
   let moduleFileNameMapping: Map<string, string>;
 
+  // 只针对 uniapp 目前选择的JS模板管理形式
   if (isCommonJSModuleKind) {
     moduleFileNameMapping = new Map();
     previousOnSubstituteNode = context.onSubstituteNode;
 
     context.onSubstituteNode = onSubstituteNode;
+
+    // 开放需要被替换的语言语法对象
     context.enableSubstitution(SyntaxKind.StringLiteral);
     context.enableSubstitution(SyntaxKind.RequireKeyword);
   }
@@ -56,6 +59,7 @@ export default function transformPathMapping(context: TransformationContext) {
     if (node.kind === SyntaxKind.Identifier) {
       let identifier = node as Identifier;
 
+      // 找到 require(...) 标记
       if (identifier.originalKeywordKind === SyntaxKind.RequireKeyword) {
         needSubstitutionModulePath = true;
         return node;
@@ -63,6 +67,7 @@ export default function transformPathMapping(context: TransformationContext) {
 
     }
 
+    // 处理和替换 require(...) 参数
     if (needSubstitutionModulePath) {
       needSubstitutionModulePath =  false;
       return substitutionModulePath(node);
@@ -80,6 +85,7 @@ export default function transformPathMapping(context: TransformationContext) {
       return node;
     }
 
+    // 危险使用，因为 resolvedModules 并不是 typescript 开放的api
     let resolvedModules = (currentSourceFile as any).resolvedModules;
 
     if (!resolvedModules) {
@@ -88,7 +94,8 @@ export default function transformPathMapping(context: TransformationContext) {
 
     let resolvedModule = resolvedModules.get(identifier.text);
 
-    if (!resolvedModule || !resolvedModule.resolvedFileName) {
+    // 忽略 node.js 核心 和 node_modules内的 模板
+    if (!resolvedModule || !resolvedModule.resolvedFileName || resolvedModule.isExternalLibraryImport) {
       return node;
     }
 
@@ -97,6 +104,7 @@ export default function transformPathMapping(context: TransformationContext) {
 
     moduleFileNameMapping.set(identifier.text, moduleFileName);
 
+    // 非推介方式实现的替换
     identifier.text = moduleFileName;
 
     return node;
